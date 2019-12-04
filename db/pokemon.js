b3f66e8cc1d00ca1ab2714e3656e6863601a687e
slothmonkeyxbox <58471360+slothmonkeyxbox@users.noreply.github.com>
2019-12-04 13:34:36 +0800

    Update pokemon.js

--- db/pokemon.js
+++ db/pokemon.js
@@ -1,28 +1,44 @@
-var pokemon = [
+var Stand= = [
     {
-      _nb:1,_engName:"The World Over Heaven",_frName:"TWOH",_type:"Close Ranged",_type2:"",_expGiven:3,_catchRate:20,_male:2,
+      _nb:1,_engName:"The World Over Heaven",_frName:"TWOH",_type:"Close Ranged",_type2:"",_expGiven:3,_catchRate:10,_male:2,
       _baseStats: {_total:700,_hp:250, _atk:75, _def:75, _sAtk:75, _sDef:75, _spd:150}
     },
     {
-      _nb:2,_engName:"Young Dio",_frName:"YOUNG DIO PART 1",_type:"Grass",_type2:"Poison",_expGiven:3,_catchRate:70,_male:2,
-      _baseStats: {_total:405,_hp:60, _atk:62, _def:63, _sAtk:80, _sDef:80, _spd:60}
+      _nb:2,_engName:"Young Dio",_frName:"YOUNG DIO PART 1",_type:"Close Ranged",_type2:"",_expGiven:3,_catchRate:70,_male:2,
+      _baseStats: {_total:150,_hp:50, _atk:20, _def:20, _sAtk:20, _sDef:20, _spd:20}
     },
     {
-      _nb:3,_engName:"Vamparic DIO",_frName:"DIO PART 1",_type:"Grass",_type2:"Poison",_expGiven:3,_catchRate:50,_male:2,
-      _baseStats: {_total:525,_hp:80, _atk:82, _def:83, _sAtk:100, _sDef:100, _spd:80},
-      _megaStats : {_total:625,_hp:80, _atk:100, _def:123, _sAtk:122, _sDef:120, _spd:80}
+      _nb:3,_engName:"Vamparic DIO",_frName:"DIO PART 1",_type:"Close Ranged",_type2:"Vampire",_expGiven:3,_catchRate:50,_male:2,
+      _baseStats: {_total:230,_hp:80, _atk:30, _def:30, _sAtk:30, _sDef:30, _spd:30}
     },
     {
-      _nb:4,_engName:"Jonathan Joestar",_frName:"Jonathan Joestar",_type:"Fire",_type2:"",_expGiven:3,_catchRate:70,_male:2,
-      _baseStats: {_total:309,_hp:39, _atk:52, _def:43, _sAtk:60, _sDef:50, _spd:65}
+      _nb:4,_engName:"Young Jonathan Joestar",_frName:"Young Jonathan Joestar",_type:"Fire",_type2:"",_expGiven:3,_catchRate:70,_male:2,
+      _baseStats: {_total:150,_hp:50, _atk:20, _def:20, _sAtk:20, _sDef:20, _spd:20}
     },
     {
       _nb:5,_engName:"Jonathan Joestar with Sword",_frName:"Jonathan Joestar",_type:"Fire",_type2:"",_expGiven:3,_catchRate:50,_male:2,
-      _baseStats: {_total:405,_hp:58, _atk:64, _def:58, _sAtk:80, _sDef:65, _spd:80}
+      _baseStats: {_total:230,_hp:80, _atk:30, _def:30, _sAtk:30, _sDef:30, _spd:30}
     },
     {
       _nb:6,_engName:"Speedwagon",_frName:"Speedwagon Foundation",_type:"Fire",_type2:"Flying",_expGiven:3,_catchRate:70,_male:2,
-      _baseStats: {_total:534,_hp:78, _atk:84, _def:78, _sAtk:109, _sDef:85, _spd:100},
+      _baseStats: {_total:150,_hp:50, _atk:20, _def:20, _sAtk:20, _sDef:20, _spd:20}
     },
+    {
+      _nb:7,_engName:"Zeppelin",_frName:"Hamon",_type:"Long Ranged",_type2:"Hamon",_expGiven:3,_catchRate:70,_male:2,
+      _baseStats: {_total:250,_hp:100, _atk:30, _def:30, _sAtk:30, _sDef:30, _spd:30}
+    },
+    {
+      _nb:8,_engName:"Joseph Joestar",_frName:"Joseph Joestar",_type:"Close Range",_type2:"Hamon",_expGiven:3,_catchRate:50,_male:2,
+      _baseStats: {_total:250,_hp:100, _atk:30, _def:30, _sAtk:30, _sDef:30, _spd:30}
+    },
+    {
+      _nb:9,_engName:"Caesar Zeppelin",_frName:"Caesar Zeppelin",_type:"Long Ranged",_type2:"Short Ranged",_expGiven:3,_catchRate:50,_male:2,
+      _baseStats: {_total:150,_hp:50, _atk:20, _def:20, _sAtk:20, _sDef:20, _spd:20}
+    },
+    {
+      _nb:10,_engName:"Caesar Zeppelin (Final Hamon)",_frName:"Caesar Hamon",_type:"Long Ranged",_type2:"Hamon",_expGiven:3,_catchRate:50,_male:2,
+      _baseStats: {_total:250,_hp:100, _atk:30, _def:30, _sAtk:30, _sDef:30, _spd:30}
+    },
+   
 ];
 module.exports = pokemon;
